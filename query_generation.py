ff1ec7b3228c7f970335eae37e2de2740c02d75e
okhrimchuk <a.okhrimchuk@gmail.com>
2020-12-13 15:03:35 +0200

    added has posted and legal_entites

--- query_generation.py
+++ query_generation.py
@@ -10,7 +10,9 @@ loc_queries = "data/queries.txt"
 vocab = pd.read_excel(loc_vocab)
 
 vocab = {k:[val for val in v if pd.notnull(val)] for k,v in vocab.to_dict(orient="list").items()}
-def generate_queries(initial_operators, join_operators, assets, clause_operators=None):
+vocab["collateral"].extend(vocab.pop("legal_entity"))
+
+def generate_queries(initial_operators, join_operators, collateral, clause_operators=None):
     queries = []
     for initial_operator in initial_operators:
         query = initial_operator
@@ -33,7 +35,16 @@ def generate_queries(initial_operators, join_operators, assets, clause_operators
                         if a < (query_len - 1):
                             body += f"{join_operator_combination[a]} "
                     queries.append(f"{query} {body}".strip())
-    return queries
+    not_has_posted_queries = [query for query in queries if re.search(r"^(?!has posted)", query)]
+    has_posted_queries = [query for query in queries if re.search(r"^(has posted)", query)]
+    joined_queries = []
+    for not_has_posted_query in not_has_posted_queries:
+        for has_posted_query in has_posted_queries:
+            joined_queries.append(not_has_posted_query + " and " + has_posted_query)
+
+    not_has_posted_queries.extend(joined_queries)
+    
+    return not_has_posted_queries
 
 queries = generate_queries(**vocab)
 with open(loc_queries, "w") as f:
